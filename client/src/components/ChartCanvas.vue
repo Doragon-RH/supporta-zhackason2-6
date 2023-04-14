49c6fecff597aedb3ef176b3a6b9fad03eca9020
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2023-04-15 03:04:36 +0900

    fix graph

--- client/src/components/ChartCanvas.vue
+++ client/src/components/ChartCanvas.vue
@@ -1,41 +1,121 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import Chart from "chart.js/auto";
-import type { ChartOptions, ChartData, ChartDataSet } from "chart.js";
+import type { ChartOptions, ChartData, ChartDataset } from "chart.js";
+import "chartjs-adapter-date-fns";
 
 const chartCanvas = ref<HTMLCanvasElement | null>(null);
+
+const img1 = new Image(20, 20);
+img1.src = "src/assets/face01.png";
+const img2 = new Image(10, 10);
+img2.src = "src/assets/face02.png";
+const img3 = new Image(30, 30);
+img3.src = "src/assets/face03.png";
+const img4 = new Image(50, 50);
+img4.src = "src/assets/face04.png";
+const img5 = new Image(40, 40);
+img5.src = "src/assets/face05.png";
 
 const renderChart = () => {
   if (chartCanvas.value) {
     const ctx = chartCanvas.value.getContext("2d");
 
     if (ctx) {
-      const chartData: ChartData<"scatter", ChartDataSet<"scatter">["data"]> = {
+      const chartData: ChartData<"scatter", ChartDataset<"scatter">["data"]> = {
         datasets: [
           {
-            label: "Sample Scatter Dataset",
             data: [
-              { x: 10, y: 20 },
-              { x: 15, y: 10 },
-              { x: 25, y: 30 },
-              { x: 35, y: 50 },
-              { x: 40, y: 40 },
+              { x: new Date("2023-04-01").valueOf(), y: 20 },
+              { x: new Date("2023-04-02").valueOf(), y: 10 },
+              { x: new Date("2023-04-08").valueOf(), y: 30 },
+              { x: new Date("2023-04-11").valueOf(), y: 50 },
+              { x: new Date("2023-04-25").valueOf(), y: 40 },
             ],
+            pointStyle: [img1, img2, img3, img4, img5],
+            pointRadius: [20, 10, 30, 50, 40],
             backgroundColor: "rgba(255, 99, 132, 0.5)",
             borderColor: "rgba(255, 99, 132, 1)",
           },
         ],
       };
 
+      const today = new Date();
+      const dates = chartData.datasets[0].data.map((point) => point.x);
+      const minDate = new Date(Math.min(...dates));
+      const maxDate = new Date(Math.max(...dates));
+
+      let min, max;
+
+      let diffDate;
+
+      if (
+        Math.abs(today.getTime() - minDate.getTime()) >
+        Math.abs(today.getTime() - maxDate.getTime())
+      ) {
+        diffDate = minDate;
+      } else {
+        diffDate = maxDate;
+      }
+      if (diffDate < today) {
+        min = diffDate;
+        max = new Date(
+          today.getTime() + (today.getTime() - diffDate.getTime())
+        );
+      } else {
+        min = new Date(
+          today.getTime() - (diffDate.getTime() - today.getTime())
+        );
+        max = diffDate;
+      }
+      min.setDate(min.getDate() - 5);
+      max.setDate(max.getDate() + 5);
+
       const chartOptions: ChartOptions<"scatter"> = {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
         scales: {
           x: {
-            type: "linear",
+            type: "time",
+            time: {
+              parser: "YYYY-MM-DD",
+              unit: "day",
+              tooltipFormat: "YYYY/MM/DD",
+            },
+            min: min.valueOf(),
+            max: max.valueOf(),
             position: "bottom",
+            ticks: {
+              callback: (value) => {
+                const date = new Date(value);
+                const diffInDays = Math.round(
+                  (date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
+                );
+                if (diffInDays === 0) {
+                  return "今日";
+                } else if (diffInDays < 0) {
+                  return Math.abs(diffInDays) + "日前";
+                } else {
+                  return diffInDays + "日後";
+                }
+              },
+              autoSkip: true,
+              autoSkipPadding: 30,
+              crossAlign: "near",
+              includeBounds: true,
+              maxRotation: 0,
+              minRotation: 0,
+              padding: 10,
+              display: true,
+              maxTicksLimit: 20,
+            },
           },
           y: {
             type: "linear",
-            position: "left",
+            position: "center",
           },
         },
       };
