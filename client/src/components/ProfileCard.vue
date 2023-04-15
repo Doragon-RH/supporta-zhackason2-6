bc174f6c8a8ab4978fe8d4aa3e354a3654a786db
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2023-04-16 04:14:45 +0900

    add upload-image

--- client/src/components/ProfileCard.vue
+++ client/src/components/ProfileCard.vue
@@ -2,14 +2,20 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import axios from "axios";
+import { createClient } from "@supabase/supabase-js";
+import { decode } from "base64-arraybuffer";
 
-const router = useRouter();
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(url, key);
 
 const email = sessionStorage.getItem("email");
 
 const uploadImg = ref();
+const fl = ref();
 const handleImageUpload = (event: any) => {
   const file = event.target.files[0];
+  fl.value = file;
   const reader = new FileReader();
   reader.onload = () => {
     uploadImg.value = reader.result;
@@ -32,20 +38,13 @@ const nameRules = [
   (v: string) => 6 <= v.length || "6文字以上で入力してください",
 ];
 
-const registerAccount = async () => {
-  await axios
-    .post("/user/signup", {
-      name: user_id.value,
-      password: password.value,
-    })
-    .then((res) => {
-      sessionStorage.setItem("user_id", res.data.id);
-      sessionStorage.setItem("user_name", res.data.name);
-      router.push("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const updateProfile = async () => {
+  const img = uploadImg.value.split(",")[1];
+  const { data } = await supabase.storage
+    .from("Images")
+    .upload(`Origin/${user_id}.png`, decode(img));
+
+  const path = data.path;
 };
 </script>
 <template>
@@ -62,58 +61,29 @@ const registerAccount = async () => {
             <v-icon v-else size="128" icon="mdi-account-circle" />
           </v-avatar>
           <div style="height: 20px" />
-          <input
-            type="file"
-            @change="handleImageUpload"
-            style="text-align: center"
-          />
+          <input type="file" @change="handleImageUpload" style="text-align: center" />
         </div>
         <div style="height: 20px" />
         <v-divider />
         <v-card-text class="pa-5">
           <v-form v-model="valid">
-            <v-text-field
-              disabled
-              v-model="user_id"
-              prepend-icon="mdi-account"
-              label="ユーザID"
-              variant="outlined"
-              required
-            />
-            <v-text-field
-              v-model="user_name"
-              :rules="nameRules"
-              prepend-icon="mdi-card-account-details"
-              label="ユーザ名"
-              variant="outlined"
-              required
-            />
+            <v-text-field disabled v-model="user_id" prepend-icon="mdi-account" label="ユーザID" variant="outlined"
+              required />
+            <v-text-field v-model="user_name" :rules="nameRules" prepend-icon="mdi-card-account-details" label="ユーザ名"
+              variant="outlined" required />
             <p class="d-flex flex-row text-subtitle-1 text-grey-darken-1">
               変形パターン
             </p>
             <div style="border: 2px solid #ccc; padding: 10px">
-              <div
-                :class="{ notSelected: selectImgType !== type }"
-                style="display: inline-block"
-                v-for="type in ['A', 'B', 'C']"
-                :key="type"
-                @click="selectImage(type)"
-              >
+              <div :class="{ notSelected: selectImgType !== type }" style="display: inline-block"
+                v-for="type in ['A', 'B', 'C']" :key="type" @click="selectImage(type)">
                 <v-avatar style="margin-right: 8px; margin-left: 8px" size="96">
-                  <v-img
-                    :src="`/src/assets/form_sample/form_sample_${type}.png`"
-                  ></v-img>
+                  <v-img :src="`/src/assets/form_sample/form_sample_${type}.png`"></v-img>
                 </v-avatar>
               </div>
             </div>
             <v-card-actions class="justify-center pa-3">
-              <v-btn
-                @click="registerAccount"
-                :disabled="!valid"
-                color="pink accent-2"
-                class="font-weight-bold text-h6"
-                flat
-              >
+              <v-btn @click="updateProfile" color="pink accent-2" class="font-weight-bold text-h6" flat>
                 登録
               </v-btn>
             </v-card-actions>
