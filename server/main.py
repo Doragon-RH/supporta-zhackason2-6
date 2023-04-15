7f2fed26a9cfb810aa1f0a6ad1cb258c0fbac6c1
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2023-04-15 22:28:08 +0900

    add base-server && add auth

---
+++ server/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+import routers
+
+app = FastAPI()
+app.include_router(routers.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="main:app",
+        host="0.0.0.0",
+        reload=True,
+        port=8888,
+        log_level="debug"
+    )
---
+++ server/routers.py
@@ -0,0 +1,43 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import APIRouter, HTTPException
+from supabase import create_client, Client
+
+import schemas
+import crud
+
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(supabase_url=url, supabase_key=key)
+
+
+router = APIRouter()
+
+
+@router.post("/auth/signup", tags=['user'])
+def signup(user: schemas.UserCreate):
+    res = supabase.auth.sign_up({
+        "email": user.email,
+        "password": user.password,
+    })
+    data, count = supabase.table('User').insert(
+        {
+            "email": res.user.email,
+            "user_id": res.user.id,
+            "user_name": "Unknown",
+        }
+    ).execute()
+    return {"user_id": res.user.id, "email": res.user.email}
+
+
+@router.post("/auth/signin", tags=['user'])
+def signin(user: schemas.UserCreate):
+    res = supabase.auth.sign_in_with_password({
+        "email": user.email,
+        "password": user.password
+    })
+    return {"id": res.user.id, "email": res.user.email}
