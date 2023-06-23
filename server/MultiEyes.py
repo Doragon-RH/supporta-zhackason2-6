5ac89a47bd7ea39f7d7fc8c6bd1b95d73dcfd226
中田愛刀 <Aito-Nakata@users.noreply.github.com>
2023-06-23 13:56:18 +0900

    部分魚眼処理完成（不自然ですが…）MultiEyes.py

--- server/MultiEyes.py
+++ server/MultiEyes.py
@@ -7,8 +7,6 @@ import os
 import fish
 import cv2
 
-
-
 def crop_image(image, x, y, width, height):
     """
     特定領域を切り抜く(-> cropped_image)
@@ -16,7 +14,7 @@ def crop_image(image, x, y, width, height):
     cropped_image = image[y:y+height, x:x+width]
     return cropped_image
 
-def processing_fish_pointed(img, name):
+def processing_fish_pointed(img):
     """
     魚眼加工
     """
@@ -26,14 +24,13 @@ def processing_fish_pointed(img, name):
 
     for i, coeff in enumerate(coefficients):
         img_fish = fish.fish(img, distortion_coefficient=coeff)
-        output = f"./tmp/A_{name}_{i+1}.png"
+        output = "result.png"
 
         n = 1+coeff
         M = cv2.getRotationMatrix2D((width//2, height//2), 0, n)
         img_resize = cv2.warpAffine(img_fish, M, (width, height))
 
         cv2.imwrite(output, img_resize)
-
 
 def paste_image(background, foreground, x, y):
     """
@@ -55,7 +52,7 @@ def paste_image(background, foreground, x, y):
         return background
 
     # 貼り付ける領域に画像を貼り付け
-    alpha = 1.0  # 画像の透明度（1.0で不透明）
+    alpha = 0  # 画像の透明度（1.0で不透明）
     blended = cv2.addWeighted(background[y:y_end, x:x_end], alpha, foreground[:fg_height, :fg_width], 1 - alpha, 0)
 
     # 背景画像に貼り付けた領域を置き換える
@@ -65,22 +62,24 @@ def paste_image(background, foreground, x, y):
 
 
 # 画像の読み込み
-image = cv2.imread("input_image.jpg")
+image = cv2.imread("face01.png")
 
 # 切り抜く範囲の座標とサイズ（例として、左上の座標が (100, 100) で、幅と高さが 200 ピクセル）
 x = 100
 y = 100
-width = 200
-height = 200
+width = 150
+height = 150
 
 # 画像の特定領域を切り抜く
 cropped_image = crop_image(image, x, y, width, height)
 
 # 魚眼加工を加える
+processing_fish_pointed(cropped_image)
 
+fish_image = cv2.imread("result.png")
 
 # 画像の貼り付け
-result_image = paste_image(image, cropped_image, x, y)
+result_image = paste_image(image.copy(), fish_image, x, y)  # 元画像のコピーを使用して貼り付け
 
 # 結果の画像を保存
 cv2.imwrite("result_image.jpg", result_image)
